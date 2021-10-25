d30b705d07c7389017f8f8881d424efb89c26dc1
Schmitz, Marcelo (ESI) <marcelo.schmitz@adp.com>
2021-10-25 13:39:55 -0300

    feat: get by type on data repo

--- src/repositories/menu-repository.js
+++ src/repositories/menu-repository.js
@@ -6,7 +6,16 @@ const Menu = moongose.model('Menu');
 const getItensFromMenu = async() => {
     return await Menu.find({
         active: true
-    });
+    }, 'description price type');
+}
+
+
+const getItensByType = async(foodType) => {
+    return await Menu.find({
+        active: true,
+        type: foodType
+    }, 'description price type');
 }
 
 exports.getItensFromMenu = getItensFromMenu;
+exports.getItensByType = getItensByType;
